64080b5aed86daf3cd086a5473251918e066461b
Cavendish02 <109635764+Cavendish02@users.noreply.github.com>
2024-04-27 19:51:53 +0300

    Update super.py

--- super.py
+++ super.py
@@ -1,69 +1,48 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[2]:
-
-
-#!/usr/bin/env python
-# coding: utf-8
-
-import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 import shap
 import matplotlib.pyplot as plt
 from pycaret.regression import setup, compare_models, finalize_model, predict_model
 
+# تعريف البيانات
 data_url = "http://lib.stat.cmu.edu/datasets/boston"
 raw_df = pd.read_csv(data_url, sep="\s+", skiprows=22, header=None)
-data = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
-target = raw_df.values[1::2, 2]
 
-X = pd.DataFrame(data, columns=["CRIM", "ZN", "INDUS", "CHAS", "NOX", "RM", "AGE", "DIS", "RAD", "TAX", "PTRATIO", "B", "LSTAT"])
-Y = pd.DataFrame(target, columns=["MEDV"])
+# تسمية الأعمدة
+feature_names = [
+    "CRIM", "ZN", "INDUS", "CHAS", "NOX", "RM", "AGE", "DIS", "RAD", "TAX",
+    "PTRATIO", "B", "LSTAT", "MEDV"
+]
+raw_df.columns = feature_names
 
-st.sidebar.header('Specify Input Parameters')
+# تقسيم البيانات
+X = raw_df.drop(columns=["MEDV"])
+y = raw_df["MEDV"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-def user_input_features():
-    features = {}
-    for feature in X.columns:
-        features[feature] = st.sidebar.slider(feature, X[feature].min(), X[feature].max(), X[feature].mean())
-    return pd.DataFrame(features, index=[0])
-
-df = user_input_features()
-
-st.header('Specified Input parameters')
-st.write(df)
-st.write('---')
-
-regression_setup = setup(data=pd.concat([X, Y], axis=1), target='MEDV')
+# إعداد النموذج باستخدام pycaret
+regression_setup = setup(data=raw_df, target="MEDV", silent=True)
 best_model = compare_models()
 final_model = finalize_model(best_model)
-final_model = final_model[-1]  # Extract the last component of the pipeline (which is the model)
-prediction = predict_model(final_model, data=df)
 
-st.header('Prediction of MEDV')
-st.write(prediction)
-st.write('---')
+# التنبؤ باستخدام النموذج النهائي
+predictions = predict_model(final_model, data=X_test)
 
+# تقييم النموذج
+from sklearn.metrics import mean_squared_error
+mse = mean_squared_error(y_test, predictions["Label"])
+print("Mean Squared Error:", mse)
+
+# شرح أهمية السمات
 explainer = shap.TreeExplainer(final_model)
 shap_values = explainer.shap_values(X)
+shap.summary_plot(shap_values, X, feature_names=X.columns)
 
-st.header('Feature Importance')
-plt.title('Feature importance based on SHAP values')
-shap.summary_plot(shap_values, X)
-st.pyplot(bbox_inches='tight')
-st.write('---')
-
-plt.title('Feature importance based on SHAP values (Bar)')
-shap.summary_plot(shap_values, X, plot_type="bar")
-st.pyplot(bbox_inches='tight')
-
-
-# In[ ]:
-
-
-
-
+# العرض التفاعلي باستخدام Streamlit
+import streamlit as st
+st.header("تقييم النموذج")
+st.write("Mean Squared Error:", mse)
+st.write("---")
+st.header("شرح أهمية السمات")
+st.pyplot(shap.summary_plot(shap_values, X, feature_names=X.columns))
